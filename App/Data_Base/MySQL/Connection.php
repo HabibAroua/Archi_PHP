d9dc45d772f3a6f3eb35f0a1332b6889c2ed972a
HabibAroua <habibha.aroua82@gmail.com>
2018-09-26 22:35:37 +0100

    dynamic of web page

--- App/Data_Base/MySQL/Connection.php
+++ App/Data_Base/MySQL/Connection.php
@@ -1,4 +1,16 @@
 <?php
-
-
+    $host='localhost'; //server address
+	$dbname='airline'; //db name
+	$username='root';	//user name 
+	$password	='';	//password
+	try
+	{	
+        $con=new PDO("mysql:host=$host;	dbname=$dbname",$username,$password);
+	    echo '<script> alert("Bienvenue cher client"); </script>';			
+	}	
+    catch(PDOException	$pe)	
+	{							
+		echo "connexion	a	la	base	a	echouee";
+	}
 ?>
+
--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,16 @@
 <?php
-    echo 'Hello World';
+    if(isset($_GET['t']))
+    {
+        $v=$_GET['t'];
+        include "../App/Views/$v.php";
+        echo $x;
+    }
+    else
+    {
+        echo 'Hello World';
+        echo '<br>';   
+    }
 ?>
+
+<p><a href="http://localhost/Archi/Public/">Lien 1</a></p><br>
+<p><a href="http://localhost/Archi/Public/?t=test">Lien 2</a></p>
