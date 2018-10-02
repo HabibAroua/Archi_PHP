e80767aa350ff08b1dd0c4f5a26ac7e8e3895b2f
HabibAroua <habibha.aroua82@gmail.com>
2018-10-02 22:30:36 +0100

    create example

--- EspaceM/index.php
+++ EspaceM/index.php
@@ -1,3 +1,5 @@
+<!-- https://www.youtube.com/watch?v=s7qtAnH5YkY -->
+<!-- 11:08-->
 <!doctype html>
 <html>
     <head>
@@ -9,7 +11,34 @@
             <h3>Inscription</h3>
             <br><br><br>
             <form method="POST" action="">
-                <input type="text" placeholder="Votre pseudo" />
+                <table>
+                    <tr>
+                        <td><label for="pseudo">Pseudo :</label></td>
+                        <td><input type="text" placeholder="Votre pseudo" name="pseudo" id="pseudo"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="mail">Mail :</label></td>
+                        <td><input type="text" placeholder="Votre Mail" name="mail" id="mail"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="confMail">Confirmation du Mail :</label></td>
+                        <td><input type="text" placeholder="Confirmer votre mail" name="confMail" id="confMail"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="motDePasse">Mot de passe :</label></td>
+                        <td><input type="text" placeholder="Confirmer votre mot de passe" name="pseudo" id="motDePasse"/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="confMotDePasse">Confirmation du mot de passe :</label></td>
+                        <td><input type="text" placeholder="Confirmer votre mot de passe" name="confMotDePasse" id="confMotDePasse"/></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td align="right"><input type="submit" value="Je m'inscris" /></td>
+                    </tr>
+                </table>
+                
+                
             </form>
         </div>
     </body>
