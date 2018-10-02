05bf3b1a27610308a47bcb7f08ef7a296926511b
HabibAroua <habibha.aroua82@gmail.com>
2018-10-02 22:11:17 +0100

    create example

---
+++ EspaceM/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Tuto Php</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div align="center">
+            <h3>Inscription</h3>
+            <br><br><br>
+            <form method="POST" action="">
+                <input type="text" placeholder="Votre pseudo" />
+            </form>
+        </div>
+    </body>
+</html>
