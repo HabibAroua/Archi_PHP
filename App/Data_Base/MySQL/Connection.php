a3c239ad1d0d1be669c6b99bbb8132d285c85c0c
HabibAroua <habibha.aroua82@gmail.com>
2018-10-06 13:01:07 +0100

    data base

--- App/Data_Base/MySQL/Connection.php
+++ App/Data_Base/MySQL/Connection.php
@@ -1,16 +1,34 @@
 <?php
-    $host='localhost'; //server address
-	$dbname='airline'; //db name
-	$username='root';	//user name 
-	$password	='';	//password
-	try
-	{	
-        $con=new PDO("mysql:host=$host;	dbname=$dbname",$username,$password);
-	    echo '<script> alert("Bienvenue cher client"); </script>';			
-	}	
-    catch(PDOException	$pe)	
-	{							
-		echo "connexion	a	la	base	a	echouee";
+	class Connection
+	{
+		private $host; //server address
+	    private $dbname; //db name
+	    private $username;	//user name 
+	    private $password;	//password
+		
+		public function __construct($host,$dbname,$username,$password)
+		{
+			$this->host=$host;
+			$this->dbname=$dbname ;
+			$this->username=$username ;
+			$this->password= $password;
+		}
+		
+		public function connect()
+		{
+		    try
+		    {
+				$con=new PDO("mysql:host=$this->host;	dbname=$this->dbname",$this->username,$this->password);
+	            echo '<script> alert("Bienvenue cher client"); </script>';
+				return true;
+	        }	
+            catch(PDOException	$pe)	
+	        {
+				return false;
+				echo "connexion	a	la	base	a	echouee";
+	        }	
+		}
+	    
 	}
 ?>
 
