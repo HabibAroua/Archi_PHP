4af6b7d9ea6d0eafc056a0b24461ad61e4502ce7
HabibAroua <habibha.aroua82@gmail.com>
2018-11-05 18:41:53 +0100

    session cokies

--- EspaceM/Exemple/login.php
+++ EspaceM/Exemple/login.php
@@ -26,7 +26,8 @@ if (isset($_POST['login']) && isset($_POST['pwd']))
 	if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd'])
 	{
 		// dans ce cas, tout est ok, on peut démarrer notre session
-
+        $lifetime = strtotime('+40 minutes', 0);
+        session_set_cookie_params($lifetime);
 		// on la démarre :)
 		session_start ();
 		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
